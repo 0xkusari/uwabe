2bb6e08d3c6fadb7373b0b808e6ed12bb7be3110
june29 <june29.jp@gmail.com>
2023-04-15 19:11:42 +0900

    Override tokenURI

--- contracts/contracts/UwabeConfigToken.sol
+++ contracts/contracts/UwabeConfigToken.sol
@@ -5,7 +5,9 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract UwabeConfigToken is ERC721, Ownable {
   using Address for address;
@@ -13,6 +15,7 @@ contract UwabeConfigToken is ERC721, Ownable {
 
   mapping(uint256 => address) public tokenIdToContract;
   mapping(uint256 => string) public tokenIdToFunctions;
+  mapping(uint256 => string) public tokenIdToSlug;
   mapping(string => uint256) public slugToTokenId;
   uint256 private fee = 0.00 ether;
   Counters.Counter private _tokenIdCounter;
@@ -33,7 +36,40 @@ contract UwabeConfigToken is ERC721, Ownable {
     _mint(msg.sender, _tokenId);
     tokenIdToContract[_tokenId] = _contract;
     tokenIdToFunctions[_tokenId] = _functions;
+    tokenIdToSlug[_tokenId] = _slug;
     slugToTokenId[_slug] = _tokenId;
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    require(_exists(_tokenId), "Non-existent Token ID");
+
+    address contractAddress = tokenIdToContract[_tokenId];
+    string memory functions = tokenIdToFunctions[_tokenId];
+    string memory slug = tokenIdToSlug[_tokenId];
+
+    return string(
+      abi.encodePacked(
+        'data:application/json;base64,',
+        Base64.encode(
+          bytes(
+            abi.encodePacked(
+              '{"name":"', tokenName(_tokenId),
+                '","description":"Otoshidama in Pochi Bukuro',
+                '","attributes":[",',
+                  '{"trait_type":"Contract","value":"', contractAddress, '"}',
+                  '{"trait_type":"Functions","value":"', functions, '"}',
+                  '{"trait_type":"Slug","value":"', slug, '"}',
+                '],"image":"https://0xkusari.github.io/uwabe/uwabe.jpg',
+              '"}'
+            )
+          )
+        )
+      )
+    );
+  }
+
+  function tokenName(uint256 _tokenId) internal pure returns(string memory) {
+    return string(abi.encodePacked("Uwabe #", Strings.toString(_tokenId)));
   }
 
   function updateFee(uint256 _fee) onlyOwner external {
