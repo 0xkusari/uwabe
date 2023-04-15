6067e541ce0fff07c226394143bbd53910bb85c6
ikd akht <picopeko@gmail.com>
2023-04-15 11:42:51 +0900

    ボタン押したら呼ぶ対象の関数を組み立てる

--- components/ContractFunctionItem.tsx
+++ components/ContractFunctionItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export interface Input {
   internalType: string;
   name: string;
@@ -23,22 +25,39 @@ export function ContractFunctionItem({
   outputs,
   readonly,
 }: ContractFunction) {
+
+  const [inputValues, setInputValues] = useState<string[]>([]);
+
+  const updateInputValues = (index: number, value: string) => {
+    const newInputValues = [...inputValues];
+    newInputValues[index] = value;
+    setInputValues(newInputValues);
+  };
+
+  const handleOnClick = (name: string) => {
+    console.log(`${name}(${inputValues.join(",")})`);
+  };
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl m-4">
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
-        {inputs.map((input) => {
+        {inputs.map((input, index) => {
           return (
             <input
               key={`${name}_${inputs.length}_${input.name}_${outputs.length}_${readonly}`}
               type="text"
               placeholder={`${input.name} (${input.type})`}
               className="input input-bordered w-full max-w-x"
+              onChange={(e) => { updateInputValues(index, e.target.value)}}
             />
           );
         })}
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">
+          <button
+            className="btn btn-primary"
+            onClick={() => handleOnClick(name)}
+          >
             {readonly ? "Query" : "Write"}
           </button>
         </div>
