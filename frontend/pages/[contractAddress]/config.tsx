3f115b3f641d748361198cca7fbae212cf5eb058
ikd akht <picopeko@gmail.com>
2023-04-16 01:57:05 +0900

    get function to expose on uwabe from config contract

--- frontend/pages/[contractAddress]/config.tsx
+++ frontend/pages/[contractAddress]/config.tsx
@@ -1,58 +1,73 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { Buffer } from "buffer";
 import {
   ContractFunction,
   ContractFunctionItem,
 } from "@/components/ContractFunctionItem";
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
-
-const configTokenAddress = '0xCeC87Fa14307f19230f754d195eD85eDC5Df5De3';
-const configTokenAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_contract","type":"address"},{"internalType":"string","name":"_functions","type":"string"},{"internalType":"string","name":"_slug","type":"string"}],"name":"buy","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"slugToTokenId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"tokenIdToContract","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"tokenIdToFunctions","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"tokenIdToSlug","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_fee","type":"uint256"}],"name":"updateFee","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+import { configTokenAddress, configTokenAbi } from "@/components/ConfigToken";
 
 export default function ConfigPage() {
-  const [contractFunctions, setContractFunctions] = useState<[] | null>(null);
   const [windowEthereum, setWindowEthereum] = useState();
-  // const [exposeFunctions, setExposeFunctions] = useState<string[]>([]);
+  const [contractFunctions, setContractFunctions] = useState<ContractFunction[]>([]);
+  const [exposeFunctions, setExposeFunctions] = useState<string[]>([]);
+  const [filteredFunctions, setFilteredFunctions] = useState<ContractFunction[]>([]);
 
   useEffect(() => {
     const { ethereum } = window as any;
     setWindowEthereum(ethereum);
+  }, []);
 
-    fetchContractData(configTokenAddress);
-  });
+  useEffect(() => {
+    fetchContractData();
+  }, [windowEthereum]);
 
-  // const fetchConfig = () => {
-  //   if (windowEthereum) {
-  //     const provider = new ethers.providers.Web3Provider(windowEthereum);
-  //     provider.send('eth_requestAccounts', []);
-  //     const signer = provider.getSigner();
-  //     const contract = new ethers.Contract(
-  //       configTokenAddress,
-  //       configTokenAbi,
-  //       provider
-  //     );
+  useEffect(() => {
+    fetchConfig(windowEthereum);
+  }, [windowEthereum, contractFunctions]);
 
-  //     const contractWithSigner = contract.connect(signer);
-  //     contractWithSigner["tokenURI"](0).then((result: any) => {
-  //       console.log("Get from Config Token");
-  //       console.table(result.toString());
-  //       // setExposeFunctions(JSON.parse(result.toString()));
-  //       setExposeFunctions(["name", "symbol", "decimals"]);
-  //     });
-  //     // contractWithSigner[name](...inputValues).then(console.table);
-  //   }
+  useEffect(() => {
+    filterFunctions(contractFunctions, exposeFunctions);
+  }, [contractFunctions, exposeFunctions]);
 
-  //   setExposeFunctions([]);
-  // };
+  const fetchConfig = (windowEthereum: any) => {
+    if (!windowEthereum) {
+      return [];
+    }
 
-  const fetchContractData = async (address: string) => {
-    const response = await fetch(`/api/contract?address=${address}`);
+    const provider = new ethers.providers.Web3Provider(windowEthereum);
+    provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      configTokenAddress,
+      configTokenAbi,
+      provider
+    );
+
+    const contractWithSigner = contract.connect(signer);
+    contractWithSigner["tokenURI"](0)
+    .then((result: any) => {
+      const decodedData = Buffer.from(result.substring(29), 'base64').toString()
+      const functions = JSON.parse(decodedData).attributes.filter((attr: any) => attr.trait_type === "Functions")[0].value.split(",")
+      setExposeFunctions(functions);
+    });
+  };
+
+  const filterFunctions = (allFunctions: ContractFunction[], exposeFunctions: string[]) => {
+    const filtered = allFunctions.filter((contractFunction: any) => {
+      return exposeFunctions.includes(contractFunction.name);
+    });
+    setFilteredFunctions(filtered);
+  };
+
+  const fetchContractData = async () => {
+    const response = await fetch(`/api/contract?address=${configTokenAddress}`);
     const data = await response.json();
     if (data.status === "1") {
       const results = JSON.parse(data.result);
       const functions = results.filter(
         (result: any) => result.type === "function"
       );
-
       const mapper = (func: any): ContractFunction => {
         return {
           name: func.name,
@@ -60,55 +75,11 @@ export default function ConfigPage() {
           outputs: func.outputs,
           readonly:
             func.stateMutability === "view" || func.stateMutability === "pure",
-          contractAddress: address,
+          contractAddress: configTokenAddress,
           abi: results,
         };
       };
-
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
-      let exposeFunctions: string[] = [];
-      if (windowEthereum) {
-        console.log("bbbbbbbbbbbbbbbbbbbbbbbbb");
-        const provider = new ethers.providers.Web3Provider(windowEthereum);
-        provider.send('eth_requestAccounts', []);
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(
-          configTokenAddress,
-          configTokenAbi,
-          provider
-        );
-        console.log("bccccccccccccccccccccccccccccccc");
-
-
-        const contractWithSigner = contract.connect(signer);
-
-        console.log("dddddddddddddd");
-
-        contractWithSigner["tokenURI"](0).then((result: any) => {
-          console.log("eeeeeeeeeeeeeeeee");
-          console.log("Get from Config Token");
-          console.table(result.toString());
-          console.log("ffffffffffffffffff")
-          // setExposeFunctions(JSON.parse(result.toString()));
-          // setExposeFunctions(["name", "symbol", "decimals"]);
-          exposeFunctions = ["buy"];
-          console.table(exposeFunctions)
-
-
-          console.log("gggggggggggg")
-        });
-        // contractWithSigner[name](...inputValues).then(console.table);
-      }
-
-      console.table(functions)
-      console.table(exposeFunctions)
-
-      const filtered = functions.filter((func: any) => exposeFunctions?.indexOf(func.name) !== -1).map(mapper);
-      console.table(filtered);
-      console.log("hhhhhhhhhhhhh")
-      setContractFunctions(filtered);
-      console.log("iiiiiiiiiiiiii")
+      setContractFunctions(functions.map(mapper));
     }
   };
 
@@ -117,7 +88,7 @@ export default function ConfigPage() {
       {contractFunctions ? (
         <div>
           <ul>
-            {contractFunctions.map((contractFunction: ContractFunction) => (
+            {filteredFunctions.map((contractFunction: ContractFunction) => (
               <li
                 key={`${contractFunction.name}_${contractFunction.inputs.length}_${contractFunction.outputs.length}_${contractFunction.readonly}`}
               >
